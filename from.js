00a3829ad360bf4ecb1d478b64b5dc64c86435d7
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-04-14 20:37:50 -0400

    clase js

---
+++ from.js
@@ -0,0 +1,42 @@
+
+document.getElementById("formulario").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let inputs = document.querySelectorAll("#formulario input", "#formulario textarea");
+    let edad = document.querySelector("input[type='Number']").value;
+    let email = document.querySelector("input[type='email']").value;
+    let valido = true;
+    let mensaje = '';
+
+    //validar campos vacios
+    inputs.forEach(function (input) {
+        if (input.value.trim() === "") {
+            valido = false;
+            mensaje = "Todos los campos deben estar completos";
+        }
+    })
+
+    //validar edad
+
+    if (edad < 18) {
+        valido = false;
+        mensaje = "Debes tener al menos 18 años";
+        console.log(">>>" + edad)
+    }
+
+
+
+    // validar correo
+    if (!email.includes("@") || !email.includes(".")) {
+        valido = false;
+        mensaje = "correo invalido";
+
+    }
+
+    if (valido) {
+        window.location.href = "valido.html"
+    }
+    else {
+        alert(mensaje);
+    }
+});
